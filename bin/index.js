1aa5343972258bb39073306c24d5d5685e2673bf
Milos Zivkovic <milos.zivkovic@mvpworkshop.co>
2022-08-05 23:00:10 +0200

    Add base flags

--- bin/index.js
+++ bin/index.js
@@ -1,56 +1,37 @@
 #!/usr/bin/env node
-'use strict';
-var __awaiter =
-    (this && this.__awaiter) ||
-    function (thisArg, _arguments, P, generator) {
-        function adopt(value) {
-            return value instanceof P
-                ? value
-                : new P(function (resolve) {
-                      resolve(value);
-                  });
-        }
-        return new (P || (P = Promise))(function (resolve, reject) {
-            function fulfilled(value) {
-                try {
-                    step(generator.next(value));
-                } catch (e) {
-                    reject(e);
-                }
-            }
-            function rejected(value) {
-                try {
-                    step(generator['throw'](value));
-                } catch (e) {
-                    reject(e);
-                }
-            }
-            function step(result) {
-                result.done
-                    ? resolve(result.value)
-                    : adopt(result.value).then(fulfilled, rejected);
-            }
-            step(
-                (generator = generator.apply(thisArg, _arguments || [])).next()
-            );
-        });
-    };
-Object.defineProperty(exports, '__esModule', { value: true });
-const commander_1 = require('commander');
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const commander_1 = require("commander");
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         const program = new commander_1.Command();
         program
-            .argument('<url>', 'Lighthouse will run the analysis on the URL.')
-            .option(
-                '-i, --iteration <type>',
-                'How many times Lighthouse should run the analysis per URL',
-                '5'
-            )
+            .name('pandoras-box')
+            .description('A small and simple stress testing tool for Ethereum-compatible blockchain clients ')
+            .version('1.0.0');
+        program
+            .requiredOption('-url, --json-rpc <json-rpc-address>', 'The URL of the JSON-RPC for the client')
+            .requiredOption('-m, --mnemonic <mnemonic>', 'The mnemonic used to generate spam accounts')
+            .option('-s, -sub-accounts <sub-accounts>', 'The number of sub-accounts that will send out transactions', '10')
+            .option('-t, --transactions <transactions>', 'The total number of transactions to be emitted', '2000')
+            .option('-m, --mode <mode>', 'The mode for the stress test. Possible modes: [EOA, ERC20, ERC721, GREETER]', 'EOA')
+            .option('-o, --output <output-path>', 'The output path for the results JSON')
             .parse();
-        const [url] = program.args;
-        const options = program.opts();
-        console.log(`url: ${url}, iteration: ${options.iteration}`);
+        // const options = program.opts();
     });
 }
-run();
+run()
+    .then()
+    .catch((err) => {
+    // TODO log errors with chalk
+    console.error(err);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,45 @@
 #!/usr/bin/env node
+import { Command } from 'commander';
 
 async function run() {
-    // Program logic
+    const program = new Command();
+
+    program
+        .name('pandoras-box')
+        .description(
+            'A small and simple stress testing tool for Ethereum-compatible blockchain clients '
+        )
+        .version('1.0.0');
+
+    program
+        .requiredOption(
+            '-url, --json-rpc <json-rpc-address>',
+            'The URL of the JSON-RPC for the client'
+        )
+        .requiredOption(
+            '-m, --mnemonic <mnemonic>',
+            'The mnemonic used to generate spam accounts'
+        )
+        .option(
+            '-s, -sub-accounts <sub-accounts>',
+            'The number of sub-accounts that will send out transactions',
+            '10'
+        )
+        .option(
+            '-t, --transactions <transactions>',
+            'The total number of transactions to be emitted',
+            '2000'
+        )
+        .option(
+            '-m, --mode <mode>',
+            'The mode for the stress test. Possible modes: [EOA, ERC20, ERC721, GREETER]',
+            'EOA'
+        )
+        .option(
+            '-o, --output <output-path>',
+            'The output path for the results JSON'
+        )
+        .parse();
 }
 
 run()
