3f78404aa702690d074b37226e697831f90fd87f
Milos Zivkovic <milos.zivkovic@mvpworkshop.co>
2022-08-05 22:37:12 +0200

    Resolve prettier issues

--- bin/index.js
+++ bin/index.js
@@ -1,22 +1,52 @@
 #!/usr/bin/env node
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const commander_1 = require("commander");
+'use strict';
+var __awaiter =
+    (this && this.__awaiter) ||
+    function (thisArg, _arguments, P, generator) {
+        function adopt(value) {
+            return value instanceof P
+                ? value
+                : new P(function (resolve) {
+                      resolve(value);
+                  });
+        }
+        return new (P || (P = Promise))(function (resolve, reject) {
+            function fulfilled(value) {
+                try {
+                    step(generator.next(value));
+                } catch (e) {
+                    reject(e);
+                }
+            }
+            function rejected(value) {
+                try {
+                    step(generator['throw'](value));
+                } catch (e) {
+                    reject(e);
+                }
+            }
+            function step(result) {
+                result.done
+                    ? resolve(result.value)
+                    : adopt(result.value).then(fulfilled, rejected);
+            }
+            step(
+                (generator = generator.apply(thisArg, _arguments || [])).next()
+            );
+        });
+    };
+Object.defineProperty(exports, '__esModule', { value: true });
+const commander_1 = require('commander');
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         const program = new commander_1.Command();
         program
             .argument('<url>', 'Lighthouse will run the analysis on the URL.')
-            .option('-i, --iteration <type>', 'How many times Lighthouse should run the analysis per URL', '5')
+            .option(
+                '-i, --iteration <type>',
+                'How many times Lighthouse should run the analysis per URL',
+                '5'
+            )
             .parse();
         const [url] = program.args;
         const options = program.opts();
