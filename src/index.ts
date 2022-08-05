f08bd8cf620400527837f016f8aee60850720b89
Milos Zivkovic <milos.zivkovic@mvpworkshop.co>
2022-08-05 22:32:56 +0200

    Initial setup commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+async function run() {
+    // Program logic
+}
+
+run()
+    .then()
+    .catch((err) => {
+        // TODO log errors with chalk
+        console.error(err);
+    });
